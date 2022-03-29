8588b44c75e5e544d322a07a918724faea91e8fa
Danyil Tuhaienko <botanick02@gmail.com>
2022-03-29 19:56:58 +0200

    MVC controller EntryNameController added

---
+++ Entry Name/Controllers/EntryNameController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+
+namespace Entry_Name.Controllers
+{
+    public class EntryHelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Entry Name/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Entry_Name.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
